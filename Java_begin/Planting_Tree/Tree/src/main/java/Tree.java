13cf6f0fcefea78f889cd9752dbc01f5c29cd227
Aigerim Raimbay <raimbay89@gmail.com>
2024-10-16 21:39:57 -0500

    tree project

---
+++ Java_begin/Planting_Tree/Tree/src/main/java/Tree.java
@@ -0,0 +1,7 @@
+public class Tree {
+    public static void main(String[] args) {
+        //We’re going to introduce ourselves and print a tree to the screen.
+        System.out.println("Hey there, I’m Ariel and I’m learning to code in Java!");
+
+    }
+}
