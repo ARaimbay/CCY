fbda52b1c431e15affd8ad851f56443bc2c3ebc5
Aigerim Raimbay <raimbay89@gmail.com>
2024-10-17 22:19:00 -0500

    declaring integer

---
+++ Java_begin/Variables/CountComment/src/main/java/CountComment.java
@@ -0,0 +1,12 @@
+public class CountComment {
+    //This is the main method that runs when you compile
+    public static void main(String[] args) {
+        int numComments = 6;
+        //This is where you will define your variable
+
+        //This is where you will print your variable
+
+    }
+
+    //This is the end of the class
+}
