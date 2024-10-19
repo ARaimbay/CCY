689e64d8237698a9ed59a15c3aadcce7c1b78105
Aigerim Raimbay <raimbay89@gmail.com>
2024-10-19 08:11:38 -0500

    profile

---
+++ Java_begin/Variables/MyProfile/src/main/java/MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        String name = "Aigerim";
+        int age = 35;
+        double desiredSalary = 200000;
+        char gender = 'f';
+        boolean lookingForJob = true;
+    }
+}
