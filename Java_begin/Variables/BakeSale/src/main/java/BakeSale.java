f3791b2d04c15a033c806225319021ed133c7b0a
Aigerim Raimbay <raimbay89@gmail.com>
2024-10-22 22:11:49 -0500

    compound operator substraction and division

--- Java_begin/Variables/BakeSale/src/main/java/BakeSale.java
+++ Java_begin/Variables/BakeSale/src/main/java/BakeSale.java
@@ -1,7 +1,8 @@
 public class BakeSale {
     public static void main(String[] args) {
         int numCookies = 17;
-
+        numCookies -= 3;
+        numCookies /= 2;
 
         // Add your code above
         System.out.println(numCookies);
