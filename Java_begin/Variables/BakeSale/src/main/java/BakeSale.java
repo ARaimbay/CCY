1436f359d937346d26eb02f254297e0b28092807
Aigerim Raimbay <raimbay89@gmail.com>
2024-10-22 22:09:05 -0500

    compound assignment operators

---
+++ Java_begin/Variables/BakeSale/src/main/java/BakeSale.java
@@ -0,0 +1,9 @@
+public class BakeSale {
+    public static void main(String[] args) {
+        int numCookies = 17;
+
+
+        // Add your code above
+        System.out.println(numCookies);
+    }
+}
