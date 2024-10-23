430c074814f555b79485e8debda20e80326dfa2c
Aigerim Raimbay <raimbay89@gmail.com>
2024-10-22 22:52:12 -0500

    comparison of another strings

--- Java_begin/Variables/EqualsSong/src/main/java/Song.java
+++ Java_begin/Variables/EqualsSong/src/main/java/Song.java
@@ -4,5 +4,6 @@ public class Song {
         String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
         String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
         System.out.println(line1.equals(line2));
+        System.out.println(line2.equals(line3));
     }
 }
