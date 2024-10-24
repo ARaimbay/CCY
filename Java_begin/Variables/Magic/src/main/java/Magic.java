c75994e7e306a22dc0471aa42378443d47894814
Aigerim Raimbay <raimbay89@gmail.com>
2024-10-24 16:56:38 -0500

    consequent 6 steps

--- Java_begin/Variables/Magic/src/main/java/Magic.java
+++ Java_begin/Variables/Magic/src/main/java/Magic.java
@@ -3,8 +3,12 @@ public class Magic {
         int myNumber = 0;
         //myNumber is an original number
         int stepOne = myNumber;
-
-
+        int stepTwo = stepOne + myNumber;
+        int stepThree = stepTwo / myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour - myNumber;
+        int stepSix = stepFive / 6;
+        System.out.println(stepSix);
 
     }
 }
