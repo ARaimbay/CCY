144b5a0d2a1766a53a3f4135bbbe3cd9dd9f7ba0
Aigerim Raimbay <raimbay89@gmail.com>
2024-10-23 23:07:20 -0500

    bank account update

---
+++ Java_begin/Variables/BankAccount/src/main/java/BankAccount.java
@@ -0,0 +1,8 @@
+public class BankAccount {
+    public static void main(String[] args){
+        double balance = 1000.75;
+        double amountToWithdraw = 250;
+        double updatedBalance = balance - amountToWithdraw;
+        double amountForEachFriend = updatedBalance / 3;
+    }
+}
