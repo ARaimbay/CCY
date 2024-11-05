069c40764aa3e333edae5b713a3f86b9156a1000
Aigerim Raimbay <raimbay89@gmail.com>
2024-11-04 22:42:14 -0600

    new instance

--- Java_begin/Classes_Objects/Store/src/main/java/Main.java
+++ Java_begin/Classes_Objects/Store/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Store lemonadeStand = new Store("lemonade", 42, .99);
+        Store lemonadeStand = new Store("lemonade");
         Store cookieShop = new Store("cookies", 12, 3.75);
 
         System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
