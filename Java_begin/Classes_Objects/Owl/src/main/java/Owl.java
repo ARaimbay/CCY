a56d4a86deef7c7cafc69f93f670b20d9b7f3a02
Aigerim Raimbay <raimbay89@gmail.com>
2024-11-08 23:10:58 -0600

    class

---
+++ Java_begin/Classes_Objects/Owl/src/main/java/Owl.java
@@ -0,0 +1,12 @@
+public class Owl {
+    String speak;
+    boolean nocturnal;
+    public Owl() {
+        speak = "Hoot";
+        nocturnal = true;
+    }
+
+    public static void main(String[] args) {
+        Owl hedwig = new Owl();
+    }
+}
