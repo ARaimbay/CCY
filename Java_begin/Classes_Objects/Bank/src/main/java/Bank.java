886a41f3305e394eadcf98ef849ec5a5003baeaf
Aigerim Raimbay <raimbay89@gmail.com>
2024-11-10 23:48:52 -0600

    constructor method

--- Java_begin/Classes_Objects/Bank/src/main/java/Bank.java
+++ Java_begin/Classes_Objects/Bank/src/main/java/Bank.java
@@ -1,4 +1,8 @@
 public class Bank {
+    int branchLocationCount;
+    public Bank(int numLocations) {
+        branchLocationCount = numLocations;
+    }
     public static void main(String[] args) {
         // program tasks
     }
