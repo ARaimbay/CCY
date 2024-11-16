3a90a4b1aa4a1541b4752b8f4fe9f47948f89061
Aigerim Raimbay <raimbay89@gmail.com>
2024-11-15 21:36:00 -0600

    parameter

---
+++ Java_begin/Classes_Objects/Shelf/src/main/java/Shelf.java
@@ -0,0 +1,12 @@
+public class Shelf {
+
+    String material;
+
+    public Shelf(String material) {
+
+    }
+
+    public static void main(String[] args) {
+        Shelf bureau = new Shelf("pine");
+    }
+}
