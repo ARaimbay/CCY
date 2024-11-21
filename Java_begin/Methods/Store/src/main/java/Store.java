5ec257d1ca28e2d9ff0fc4c30164dc96a8cc81d5
Aigerim Raimbay <raimbay89@gmail.com>
2024-11-20 21:34:35 -0600

    store class

---
+++ Java_begin/Methods/Store/src/main/java/Store.java
@@ -0,0 +1,13 @@
+public class Store {
+    // instance fields
+    String productType;
+
+    // constructor method
+    public Store(String product) {
+        productType = product;
+    }
+
+    // Add advertise method below
+
+
+}
