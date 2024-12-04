f7e241d17f92d201d85f6176b9572f4c5c6cada8
Aigerim Raimbay <raimbay89@gmail.com>
2024-12-03 21:10:01 -0600

    increase of price

--- Java_begin/Methods/Store/src/main/java/Main.java
+++ Java_begin/Methods/Store/src/main/java/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Store lemonadeStand = new Store("Lemonade");
+        Store lemonadeStand = new Store("Lemonade", 3.75 );
         lemonadeStand.advertise();
         lemonadeStand.greetCustomer("Codey");
+        lemonadeStand.increasePrice(1.50);
+        System.out.println(lemonadeStand.price);
 
     }
 }
