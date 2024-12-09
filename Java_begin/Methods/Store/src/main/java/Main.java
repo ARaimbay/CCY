762476ecff82978b8bacfc868ec8986350687072
Aigerim Raimbay <raimbay89@gmail.com>
2024-12-09 14:57:04 -0600

    new object

--- Java_begin/Methods/Store/src/main/java/Main.java
+++ Java_begin/Methods/Store/src/main/java/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Store lemonadeStand = new Store("Lemonade", 3.75 );
+        Store cookieShop = new Store("Cookies", 5);
         lemonadeStand.advertise();
         lemonadeStand.greetCustomer("Codey");
         lemonadeStand.increasePrice(1.50);
         System.out.println(lemonadeStand.price);
         double lemonadePrice = lemonadeStand.getPriceWithTax();
         System.out.println(lemonadePrice);
+        System.out.println(cookieShop);
 
     }
 }
