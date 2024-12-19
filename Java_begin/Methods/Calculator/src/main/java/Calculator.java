e9897db9e25cb6f69588d6a7968a14ad0bd06987
Aigerim Raimbay <raimbay89@gmail.com>
2024-12-19 11:40:28 -0600

    3 methods

--- Java_begin/Methods/Calculator/src/main/java/Calculator.java
+++ Java_begin/Methods/Calculator/src/main/java/Calculator.java
@@ -1,11 +1,16 @@
 public class Calculator {
-    public Calculator(int a, int b) {
-        public int add(int a, int b);
-            add = a + b;
-            return add;
-        public int substract(int a, int b){
-            substract = a-b;
-            return substract;
-        }
+    public Calculator() {
+        int add(int a, int b);
+        int add = a + b;
+        return add;
+    }
+    public int substract(int a, int b){
+        int substract = a-b;
+        return substract;
+
+    }
+    public int multiply(int a, int b){
+        int multiply = a*b;
+        return multiply;
     }
 }
