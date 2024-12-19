04b94c97591cb2d27478bcc85dea99c64d5a255c
Aigerim Raimbay <raimbay89@gmail.com>
2024-12-18 18:16:34 -0600

    substract

--- Java_begin/Methods/Calculator/src/main/java/Calculator.java
+++ Java_begin/Methods/Calculator/src/main/java/Calculator.java
@@ -1,7 +1,10 @@
 public class Calculator {
     public Calculator(int a, int b) {
         int add(int a, int b);
-        add = a + b;
-        return add;
+            add = a + b;
+            return add;
+        public int substract(int a, int b){
+            substract = a-b;
+        }
     }
 }
