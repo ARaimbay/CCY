4bae0efd4ed6751db0eba4cb7e7707d4e176ee35
Aigerim Raimbay <raimbay89@gmail.com>
2025-01-01 12:22:43 -0600

    modulo

--- Java_begin/Methods/Calculator/src/main/java/Calculator.java
+++ Java_begin/Methods/Calculator/src/main/java/Calculator.java
@@ -20,6 +20,7 @@ public class Calculator {
     public static void main() {
         System.out.print(add(5, 3));
         System.out.print(substract(45, 11));
+        System.out.println(modulo(2));
 
     }
 
