f6f5ee901dfb34c9af2f7178fbc7d7fd09710ac5
Aigerim Raimbay <raimbay89@gmail.com>
2025-01-23 12:12:39 -0600

    new modulo method

--- Java_begin/Methods/Calculator/src/main/java/Calculator.java
+++ Java_begin/Methods/Calculator/src/main/java/Calculator.java
@@ -47,6 +47,7 @@ public class Calculator {
         System.out.print(modulo(5));
         System.out.println(add(2, 3));
         System.out.print(multiply(2, 3));
+        System.out.print(modulo(2));
 
 
     }
