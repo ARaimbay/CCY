ff1ee52f34725969fd8b6937aabc99733485323b
Aigerim Raimbay <raimbay89@gmail.com>
2025-02-17 18:16:07 -0600

    divide parameter

--- Java_begin/Methods/Calculator/src/main/java/Calculator.java
+++ Java_begin/Methods/Calculator/src/main/java/Calculator.java
@@ -42,7 +42,7 @@ public class Calculator {
         System.out.println(add(2, 1));
         System.out.println(modulo(1));
         System.out.print(substract(5,3));
-        System.out.println(divide(4, 4));
+        System.out.println(divide(4, 2));
         System.out.print(multiply(1, 5));
         System.out.print(modulo(5));
         System.out.println(add(3, 3));
